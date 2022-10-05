a881689d2919850a9c03fe63f42556ff9c6aaa62
Nancy Yoder <nayo4039@colorado.edu>
2022-10-05 13:43:35 -0600

    code for milestone 2 (Almost MVP)

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let titleText = document.getElementById('titleText');
+
+setTimeout(() => {titleText.innerHTML = "CLICK ON THE ORANGE FLOWER TO COLLECT ITS POLLEN"}, 2000);
+
 let bumbleBee = document.getElementById('bumbleBee');
 
 const onMouseMove = (e) =>{
@@ -6,14 +10,26 @@ const onMouseMove = (e) =>{
 }
 document.addEventListener('mousemove', onMouseMove);
 
-let titleText = document.getElementById('titleText');
-
 let orangeFlower = document.getElementById('orangeFlower');
 
 let orangeCounter = 0;
 let pinkCounter = 0;
 
 orangeFlower.addEventListener('click', () => {
+    if(orangeCounter === 0) {
+        document.getElementById("orangeImg").src="orangeFlower2.png";
+        titleText.innerHTML = "BEES COLLECT POLLEN BECAUSE ... ";
+    }
+    else if(orangeCounter === 1) {
+        document.getElementById("orangeImg").src="orangeFlower3.png";
+    }
+    else if(orangeCounter === 2) {
+        document.getElementById("orangeImg").src="orangeFlower4.png";
+    }
+    else if(orangeCounter === 3) {
+        document.getElementById("orangeImg").src="orangeFlower5.png";
+        titleText.innerHTML = "CLICK ON THE PINK FLOWER TO DISTRIBUTE THE POLLEN";
+    }
     orangeCounter++;
     console.log("orange counter", orangeCounter);
 })
@@ -21,9 +37,9 @@ orangeFlower.addEventListener('click', () => {
 let pinkFlower = document.getElementById('pinkFlower');
 
 pinkFlower.addEventListener('click', () => {
-    titleText.innerHTML = "PINK FLOWER CLICKED";
     if(pinkCounter === 0) {
         document.getElementById("pinkImg").src="pinkFlower2.png";
+        titleText.innerHTML = "BEES DISTRIBUTE POLLEN BECAUSE ... ";
     }
     else if(pinkCounter === 1) {
         document.getElementById("pinkImg").src="pinkFlower3.png";
@@ -33,6 +49,17 @@ pinkFlower.addEventListener('click', () => {
     }
     else if(pinkCounter === 3) {
         document.getElementById("pinkImg").src="pinkFlower5.png";
+        titleText.innerHTML = "WATCH THE NEW FLOWERS GROW BECAUSE OF THE POLLINATION";
+
+        
+        setTimeout(() => {
+            let purpleFlower = document.getElementById('purpleFlower');
+            purpleFlower.style.visibility = "visible";
+            let redFlower = document.getElementById('redFlower');
+            redFlower.style.visibility = "visible";
+        }, 1000);
+        
+        
     }
     pinkCounter++;
     console.log("pink counter", pinkCounter);
