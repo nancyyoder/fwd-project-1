1dec0297d98c8c1bd81cb30cf4443032aeb8fbd6
Nancy Yoder <nayo4039@colorado.edu>
2022-10-05 09:51:16 -0600

    added way more stuff #slay

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-console.log("hello im here")
-
 let bumbleBee = document.getElementById('bumbleBee');
 
 const onMouseMove = (e) =>{
-    console.log("inside");
   bumbleBee.style.left = e.pageX + 'px';
   bumbleBee.style.top = e.pageY + 'px';
 }
 document.addEventListener('mousemove', onMouseMove);
+
+let titleText = document.getElementById('titleText');
+
+let orangeFlower = document.getElementById('orangeFlower');
+
+let orangeCounter = 0;
+let pinkCounter = 0;
+
+orangeFlower.addEventListener('click', () => {
+    orangeCounter++;
+    console.log("orange counter", orangeCounter);
+})
+
+let pinkFlower = document.getElementById('pinkFlower');
+
+pinkFlower.addEventListener('click', () => {
+    titleText.innerHTML = "PINK FLOWER CLICKED";
+    if(pinkCounter === 0) {
+        document.getElementById("pinkImg").src="pinkFlower2.png";
+    }
+    else if(pinkCounter === 1) {
+        document.getElementById("pinkImg").src="pinkFlower3.png";
+    }
+    else if(pinkCounter === 2) {
+        document.getElementById("pinkImg").src="pinkFlower4.png";
+    }
+    else if(pinkCounter === 3) {
+        document.getElementById("pinkImg").src="pinkFlower5.png";
+    }
+    pinkCounter++;
+    console.log("pink counter", pinkCounter);
+
+
+
+
+})
